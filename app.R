33aab148a5957c9bc2bac076315e8ce397b468bd
HuaiLin Tang <huailintang@HuaiLins-MacBook-Air.local>
2022-03-13 22:26:08 -0500

    last touch

---
+++ app.R
@@ -0,0 +1,82 @@
+library(dplyr)
+library(shiny)
+library(shinyWidgets)
+library(ggplot2)
+library(tidyr)
+library(ggrepel)
+
+fertility_rate <- read.csv("birthrate.csv")
+region <- read.csv("region.csv")
+
+table <- merge(x=fertility_rate,y=region,
+               by.x="STATE", by.y="State.Code",all.x=TRUE) %>% 
+  select(-c(STATE,BIRTHS,URL,Division)) %>% 
+  rename(Year = YEAR, Fertility_rate = FERTILITY.RATE)
+
+table$Year = table$Year - 2000 #turn year 2015 to 15
+
+table = table %>% 
+  filter(Year %in% 14:20) %>%  #only use fertility_rate from 14 to 20
+  arrange(desc(Fertility_rate)) #rank fertility_rate
+
+state = table %>% 
+  pull(State) %>% 
+  unique() #pull state with fertility rate from high to low
+
+region <- table %>% 
+  pull(Region) %>% 
+  unique()
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      # select region
+      pickerInput("Region_input", "Region", choices = region, selected = region, 
+                  options = list(`actions-box` = TRUE), multiple = TRUE),
+      # select state
+      pickerInput("State_input", "State", choices = state, selected = state, 
+                  options = list(`actions-box` = TRUE), multiple = TRUE), 
+      width = 2
+    ),
+    mainPanel(
+      plotOutput("graph_1", width = "1150px", height = "900px"),
+      dataTableOutput("table_1")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  table_1 <- reactive({
+    table %>% 
+      filter(Region %in% input$Region_input) %>% 
+      filter(State %in% input$State_input)
+  })
+  
+  observeEvent(input$Region_input,{
+    # update state based on region input
+    updatePickerInput(session, "State_input", choices = unique(table_1()$State),
+                      selected = unique(table_1()$State))
+  })
+  
+  output$graph_1 <- renderPlot({
+    ggplot(table_1(), aes(x=Year, y=Fertility_rate, # color line by region
+                          group=interaction(State, Region), color=Region)) + 
+      geom_line() +       
+      theme(legend.position = "bottom", axis.title = element_text(size = 14), axis.text = element_text(size =10)) +
+      title("Fertility rate from 2014-2020") +
+      ylab("Fertility rate") +
+      scale_color_brewer(palette = "Set2") +
+      geom_text_repel(aes(label = State))
+    # geom_dl(aes(label = State), 
+    #         method = list(dl.trans(x = x - .2), "last.points"), 
+    #         list('last.bumpup')) +
+    # geom_dl(aes(label = State), method = list(dl.trans(x = x + .2), 
+    #                                       "first.points")) 
+  }, height = 900, width = 1150)
+  
+  output$table_1 <- renderDataTable({
+    table_1()
+  })
+}
+
+app <- shinyApp(ui, server)
